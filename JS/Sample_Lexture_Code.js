6d4c1d9aaf64d666e22709523d5a985b8674a4aa
katoltmans <katoltmans@msn.com>
2022-01-25 11:55:51 -0500

    Add JavaScript Folder

---
+++ JS/Sample_Lexture_Code.js
@@ -0,0 +1,35 @@
+var banana = 60
+let y = 2
+const z = 75
+
+//console.log(banana)       //diplays 60
+
+banana = 75     //changes banana to 75
+banana += 10    //adds 10
+banana ++       //adds 1
+banana -= 5       //subtracts 5
+
+//console.log(banana)       //diplays 81
+
+var age = 43          //int
+var pie = 3.14        //float
+var _name = "Kat"     //string (added _ to name because name is preassigned)
+var isHuman = true    //boolean
+
+var hobbies = ["family time", "fitness", "dance", "swimming"]   //array
+
+var human = {
+    name: "Kat",
+    age: 43,
+    isHuman: true,
+    hobbies: ["family time", "fitness", "dance", "swimming"]
+}   //object
+
+//console.log(human)                  //displays all data in human
+//console.log(human.name, human.age)  //displays name and age of human - Kat 43
+//console.log("name:", human.name, "age:", human.age)  //name: Kat age: 43
+//console.log('Name: ${human.name}, Age: ${humand.age}')
+
+//console.log(human.hobbies[2])       ////diplays dance
+
+function chaneToBlue ()
