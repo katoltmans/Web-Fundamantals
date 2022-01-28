3845c029b70611d789726a121f8aaa6395a719b9
katoltmans <katoltmans@msn.com>
2022-01-28 10:58:19 -0500

    Turn-in ready!

--- JS/DojoWeather/script.js
+++ JS/DojoWeather/script.js
@@ -7,21 +7,20 @@ function removeAlert(id) {
 }
 
 function convertDegree (element, convert) {
-    console.log("hi");
+    
     var tempArray = document.querySelectorAll(convert);
+
     for(var i=0; i < tempArray.length; i++) {
         var number = parseInt(tempArray[i].innerText);
         if (element.value == "celsius") {
             celsius = (number-32)*(5/9);
             celsius = Math.round(celsius);
             tempArray[i].innerText = celsius
-            console.log(celsius);
             }
         else {
             fahrenheit = (number*(9/5) + 32);
             fahrenheit = Math.round(fahrenheit);
             tempArray[i].innerText = fahrenheit
-            console.log(fahrenheit);
             }
     }
 
