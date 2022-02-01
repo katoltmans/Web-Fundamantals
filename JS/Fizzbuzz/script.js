4379ca7a2097b6095045a4da9055404dc985830a
katoltmans <katoltmans@msn.com>
2022-02-01 16:16:21 -0500

    initial commit

---
+++ JS/Fizzbuzz/script.js
@@ -0,0 +1,21 @@
+//Fixxbuzz problem - iterate through numbers 1-100
+//Numbers divisible by 3 console.log "Fizz"
+//Numbers divisible by 5 console.log "Buzz"
+//NUmbers divisible by both 3 and 5 console.log "FizzBuzz"
+//All other numbers print themselves
+
+for(i=1; i<=100; i++) {
+
+    if(i%3==0 && i%5==0) {
+        console.log("FizzBuzz")
+    }
+    else if(i%5==0) {
+        console.log("Buzz")
+    }
+    else if(i%3==0 ) {
+        console.log("Fizz")
+    }
+    else {
+        console.log(i)
+    }
+}
