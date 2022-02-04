8dc18025b917215442b57ce0c6120b5a8eda6219
katoltmans <katoltmans@msn.com>
2022-02-04 18:33:14 -0500

    add operator funationality

--- JS/Calculator/script.js
+++ JS/Calculator/script.js
@@ -1,10 +1,23 @@
 //calculate result
 var result = 0;
 var operator = "+";
+var numLeft = 0;
+var numRight = 0;
+
 function press(num) {
     if(operator == "+") {
         result += num;
     }
+    else if(operator == "-") {
+        result -= num;
+    }
+    else if(operator == "*") {
+        result *= num;
+    }
+    else if(operator == "/") {
+        result /= num;
+    }
+    
     document.getElementById("display").innerText = result;
 }
 
