ba5cc2afc4c5997c4ac3ac51035c5b7f7ee4ac87
katoltmans <katoltmans@msn.com>
2022-02-04 19:09:26 -0500

    working calculator

--- JS/Calculator/script.js
+++ JS/Calculator/script.js
@@ -1,26 +1,48 @@
 //calculate result
 var result = 0;
-var operator = "+";
-var numLeft = 0;
-var numRight = 0;
+var numLeft = "";
+var numRight = "";
+var operatorPress = false;
+var operator = "";
 
 function press(num) {
-    if(operator == "+") {
-        result += num;
+    if(operatorPress == true) {
+        numRight += num;
+        document.getElementById("display").innerText = numRight;
     }
-    else if(operator == "-") {
-        result -= num;
+    else{
+        numLeft += num;
+        document.getElementById("display").innerText = numLeft;
     }
-    else if(operator == "*") {
-        result *= num;
-    }
-    else if(operator == "/") {
-        result /= num;
-    }
-    
-    document.getElementById("display").innerText = result;
 }
 
 function setOP(op) {
+    operatorPress = true;
     operator = op;
+}
+
+function clr() {
+    result = 0;
+    numLeft = "";
+    numRight = "";
+    operatorPress = false;
+    operator = "";
+    document.getElementById("display").innerText = result;
+}
+
+function calculate () {
+    if(operator == "+") {
+        result = parseFloat(numLeft) + parseFloat(numRight);
+    }
+    else if(operator == "-") {
+        result = parseFloat(numLeft) - parseFloat(numRight);
+    }
+    else if(operator == "*") {
+        result = parseFloat(numLeft) * parseFloat(numRight);
+    }
+    else if(operator == "/") {
+        result = parseFloat(numLeft) / parseFloat(numRight);
+    }
+    
+    document.getElementById("display").innerText = result;
 }
