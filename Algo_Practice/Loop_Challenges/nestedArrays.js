a3f1e440a34aad87e6e993e4045caa4545b8c71b
katoltmans <katoltmans@msn.com>
2022-05-31 13:06:59 -0400

    Initial commit

---
+++ Algo_Practice/Loop_Challenges/nestedArrays.js
@@ -0,0 +1,18 @@
+var arr2d = [
+    [2, 5, 8],
+    [3, 6, 1],
+    [5, 7, 7],
+];
+
+// We can console.log the `8` in this array if we
+console.log(arr2d[0][2]);
+// the first index `0` will select the `[2, 5, 8]` sub-array
+// the second index `2` will select the `8` out of that sub-array
+
+// const isPresent2d = (arr, value) => {
+//     for (i=0; i<arr.length; i++) {
+//         for (n=0, n<arr[i].length, n++) {
+
+//         }
+//     }
+// }
